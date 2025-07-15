142ec88490c1912d0117923ace9f41cfcd8f5001
Noah-Oliver <80138199+Noah-Oliver@users.noreply.github.com>
2025-07-15 15:58:30 +0800

    Update clash.js

--- config/clash.js
+++ config/clash.js
@@ -44,6 +44,7 @@ function main(config) {
   config["dns"] = {
     enable: true,
     "cache-algorithm": "arc",
+    listen: "0.0.0.0:1053"
     ipv6: true,
     "enhanced-mode": "redir-host",
     "default-nameserver": ["223.5.5.5","119.29.29.29"],
@@ -58,7 +59,7 @@ function main(config) {
     stack: "system",
     "auto-route": true,
     "auto-detect-interface": true,
-    "dns-hijack": [],
+    "dns-hijack": ["any:53","tcp://any:53"],
     mtu: 9000
   }
   config["sniffer"] = {
