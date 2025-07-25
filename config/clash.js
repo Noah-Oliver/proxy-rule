d8dbda43c8d2bf3a3e714301ad701e5fffe4fc9b
Noah-Oliver <80138199+Noah-Oliver@users.noreply.github.com>
2025-07-25 22:01:35 +0800

    Update clash.js

--- config/clash.js
+++ config/clash.js
@@ -47,8 +47,8 @@ function main(config) {
     listen: "0.0.0.0:1053",
     ipv6: true,
     "enhanced-mode": "redir-host",
-    "default-nameserver": ["223.5.5.5","119.29.29.29"],
-    "nameserver": ["https://dns.alidns.com/dns-query","https://doh.pub/dns-query"],
+    "default-nameserver": ["dhcp://system"],
+    "nameserver": ["dhcp://system"],
     "nameserver-policy": {
       //PROXY
       "rule-set:0proxy,gfw,cn!,tld-!cn": ["https://dns.cloudflare.com/dns-query#PROXY","https://dns.google/dns-query#PROXY"],
