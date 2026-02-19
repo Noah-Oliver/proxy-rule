fcf5dfa8f20eeffa5a737cb11618f62961249b12
Noah-Oliver <80138199+Noah-Oliver@users.noreply.github.com>
2026-02-19 19:18:38 +0800

    Update clash.js

--- config/clash.js
+++ config/clash.js
@@ -58,11 +58,11 @@ function main(config) {
     "cache-algorithm": "arc",
     ipv6: true,
     "enhanced-mode": "redir-host",
-    "default-nameserver": ["udp://223.5.5.5#国内", "udp://119.29.29.29#国内"],
-    "nameserver": ["udp://223.5.5.5#国内", "udp://119.29.29.29#国内"],
+    "default-nameserver": ["tls://223.5.5.5#国内", "tls://119.29.29.29#国内"],
+    "nameserver": ["tls://223.5.5.5#国内", "tls://119.29.29.29#国内"],
     "nameserver-policy": {
       //PROXY
-      "rule-set:unlock,proxy,cn!": ["udp://8.8.8.8#国外", "udp://1.1.1.1#国外"],
+      "rule-set:unlock,proxy,cn!": ["tls://8.8.8.8#国外", "tls://1.1.1.1#国外"],
     },
   }
 
