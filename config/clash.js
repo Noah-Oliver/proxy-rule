766af0e7c3f07ca0c68a4135278ef439e2d0a811
Noah-Oliver <80138199+Noah-Oliver@users.noreply.github.com>
2026-03-04 21:32:12 +0800

    Update clash.js

--- config/clash.js
+++ config/clash.js
@@ -57,7 +57,7 @@ function main(config) {
     enable: true,
     ipv6: true,
     "enhanced-mode": "redir-host",
-    "nameserver": ["223.5.5.5"],
+    "nameserver": ["119.29.29.29"],
   }
 
   config["tun"] = {
